220c00f4e04de2be0b5ca06c2378ce47c7c4c55e
parker <parkerlewis2021@gmail.com>
2024-05-14 16:58:39 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return "This Saturday, I want to " + activity + "!";
+  }
+console.log(saturdayFun("bathe my dog"));
+
+function mondayWork(activity = "go to the office"){
+    return "This Monday, I will " + activity + ".";
+}
+console.log(mondayWork("work from home"))
+
+function wrapAdjective(niceSymbol = '*'){
+    return function (niceWord = "special"){
+        return `You are ${niceSymbol}${niceWord}${niceSymbol}!`
+    }
+}
+
